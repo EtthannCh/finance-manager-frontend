aa2ca3ca2cea6a95cfdd13152919b6c8070b6dc7
etthannch <etthannch@gmail.com>
2026-06-18 21:07:36 +0700

    fix: import wrong type for inventory iteem

--- app/inventory/inventory-item/columns.tsx
+++ app/inventory/inventory-item/columns.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { InventoryItem } from "./page";
+import { InventoryItemType } from "./page";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-export const columns: ColumnDef<InventoryItem>[] = [
+export const columns: ColumnDef<InventoryItemType>[] = [
   {
     accessorKey: "materialItemName",
     header: "Material Item Name",
